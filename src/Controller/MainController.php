b853c2ab2a786befc2cffff53dbf967ffd7bd936
Jo <joffrey.biteau@viacesi.fr>
2020-02-29 16:33:23 +0100

    fix bug route 404: ajout .htaccess basic

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\SpotRepository;
+use App\Repository\UserRepository;
 use DateTime;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="_index")
+     * @Route("/", name="index")
      * @param Request $request
      * @return JsonResponse
      * @throws Exception
@@ -21,9 +23,34 @@ class MainController extends AbstractController
     {
         $data = json_decode($request->getContent(), true);
 
-        if (isset($data['date']))  {
+        if (isset($data['date'])) {
             $date = new DateTime($data['date']);
 
+        } else {
+            $date = new DateTime();
+        }
+
+        $result = ["hello" => $date];
+
+        return $this->json($result);
+    }
+
+    /**
+     * @Route("/addspots", name="add_spot")
+     * @param Request $request
+     * @param UserRepository $userRepo
+     * @return JsonResponse
+     * @throws Exception
+     */
+        public function addSpots(Request $request, UserRepository $userRepo)
+    {
+        dd("ok add");
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['user']))  {
+            $user = $userRepo->findOneBy(['name'=>$data['user']]);
+
+            dd($user);
         } else {
             $date = new DateTime();
         }
